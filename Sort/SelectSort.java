1f8c9c2eb9c42f8de80bdebbf1c63f98c6a30b54
mbfczzz <a317242392@163.com>
2021-06-24 01:11:48 +0800

    2021-6-24

--- Sort/SelectSort.java
+++ Sort/SelectSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  * 选择排序
  * 先排序
  *  保存每一次遍历的最小值   找到最小值 进行交换
+ *  多个最小值 找到最小的哪一个
  *  稳定的  o(n²) o(1)
  * 2021/6/23
  */
@@ -19,8 +20,8 @@ public class SelectSort {
         for (int j=0;j< array.length;j++){
             int min = j;
             for (int i=j+1;i< array.length;i++){
-                if(array[i]<array[j]){
-                    min = i;
+                if(array[i]<array[min]){
+                        min = i;
                 }
             }
             int tmp = array[j];
@@ -31,7 +32,7 @@ public class SelectSort {
     }
 
     public static void main(String args[]){
-        int[] array =  selectionSort(new int[]{1,4,52,4,2});
+        int[] array =  selectionSort(new int[]{6,8,2,1,4,52,4,2});
         System.out.println(Arrays.toString(array));
     }
 }
