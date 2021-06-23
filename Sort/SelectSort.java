c22365d5129f571b3557302c22ed7f59c79e8687
mbfczzz <a317242392@163.com>
2021-06-24 00:47:35 +0800

    2021-6-24

---
+++ Sort/SelectSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+
+/**
+ * 选择排序
+ * 先排序
+ *  保存每一次遍历的最小值   找到最小值 进行交换
+ *  稳定的  o(n²) o(1)
+ * 2021/6/23
+ */
+public class SelectSort {
+
+    private static int[]  selectionSort(int[] array){
+        if(array.length<=1){
+            return array;
+        }
+        //未排序区间开始排序
+
+        for (int j=0;j< array.length;j++){
+            int min = j;
+            for (int i=j+1;i< array.length;i++){
+                if(array[i]<array[j]){
+                    min = i;
+                }
+            }
+            int tmp = array[j];
+            array[j] = array[min];
+            array[min] = tmp;
+        }
+        return array;
+    }
+
+    public static void main(String args[]){
+        int[] array =  selectionSort(new int[]{1,4,52,4,2});
+        System.out.println(Arrays.toString(array));
+    }
+}
